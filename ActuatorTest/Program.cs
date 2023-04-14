af8e4fd7a088341158516f20cd3b7b4059451661
salvationlloydstanley-kemuel <skemuel008@gmail.com>
2023-04-14 13:12:44 +0100

    First commit

---
+++ ActuatorTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using Steeltoe.Extensions.Configuration.ConfigServer;
+using Steeltoe.Management.Endpoint;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.AddConfigServer();
+
+// builder.Configuration.AddConfigServer();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAllActuators(builder.Configuration);
+builder.Services.ActivateActuatorEndpoints();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
